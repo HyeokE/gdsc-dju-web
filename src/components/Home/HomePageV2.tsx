9af90abccb3fa04d418a8aa76ddc0c60e4fcd9cc
HYEOK_E <jhjeong00@gmail.com>
2022-05-05 03:15:52 +0900

    chore: modify mainText

--- src/components/Home/HomePageV2.tsx
+++ src/components/Home/HomePageV2.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {
   BannerTitleWrapper,
+  ButtonWrapper,
   HomeWrapper,
   MainBannerText,
   RecruitingWrapper,
+  StyledRecruitmentButton,
 } from '../../pages/Home/styled';
 import { bannerItemAnimate } from '../common/Variants/Variants';
 import Recruiting from '../../assets/Recruiting';
 import SolarSystem from '../../components/Home/SolorSystem';
+import { useRecoilState } from 'recoil';
+import { recruitmentState } from '../../store/recruitHandler';
 
 const HomePageV2 = () => {
   return (
@@ -17,14 +21,35 @@ const HomePageV2 = () => {
         <RecruitingWrapper variants={bannerItemAnimate}>
           <Recruiting />
           <MainBannerText variants={bannerItemAnimate}>
-            상상을 현실로
+            상상을 현실로 바꾸다.
           </MainBannerText>
-          <MainBannerText variants={bannerItemAnimate}>
-            11. 22 ~ 12. 19
-          </MainBannerText>
+          <HomeRecruitmentButton />
+          {/*<MainBannerText variants={bannerItemAnimate}>*/}
+          {/*  11. 22 ~ 12. 19*/}
+          {/*</MainBannerText>*/}
         </RecruitingWrapper>
       </BannerTitleWrapper>
     </HomeWrapper>
+  );
+};
+const HomeRecruitmentButton = () => {
+  const [recruit] = useRecoilState(recruitmentState);
+  return (
+    <ButtonWrapper variants={bannerItemAnimate}>
+      {recruit.home ? (
+        <StyledRecruitmentButton
+          onClick={() => {
+            window.open('https://gdsc-dju.web.app/recruit', '_blank');
+          }}
+        >
+          지원하기
+        </StyledRecruitmentButton>
+      ) : (
+        <StyledRecruitmentButton disable={true}>
+          지원기간이 아닙니다.
+        </StyledRecruitmentButton>
+      )}
+    </ButtonWrapper>
   );
 };
 
