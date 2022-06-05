a7565366e3f608f54c2428cb1a9f300dd0e23945
HYEOK_E <jhjeong00@gmail.com>
2022-06-05 20:28:08 +0900

    chore: modify applicantsCard

--- src/components/admin/ApplicantCard/styled.ts
+++ src/components/admin/ApplicantCard/styled.ts
@@ -4,15 +4,29 @@ import { motion } from 'framer-motion';
 export const ApplicantCardWrapper = styled(motion.div)`
   display: flex;
   flex-direction: row;
-  padding: 17px 30px;
+  padding: 10px 30px;
   justify-content: space-around;
   box-shadow: 0 2px 12px 0 ${({ theme }) => theme.colors.grey300};
   border-radius: 10px;
   box-sizing: border-box;
   align-items: center;
   cursor: pointer;
+  height: 50px;
   width: 400px;
 `;
 export const ApplicantText = styled(motion.div)`
   font-size: ${({ theme }) => theme.fontSize.body2};
+  text-overflow: ellipsis;
+  width: 80px;
+  white-space: nowrap;
+  overflow: hidden;
+  display: block;
+`;
+export const ApplicantEmail = styled(motion.div)`
+  font-size: ${({ theme }) => theme.fontSize.body2};
+  text-overflow: ellipsis;
+  width: 100px;
+  white-space: nowrap;
+  overflow: hidden;
+  display: block;
 `;
