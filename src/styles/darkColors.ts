f0abd8757157666885c0b69e21c4c4620942c8bf
HyeokE <jhjeong00@gmail.com>
2022-06-26 16:52:19 +0900

    feat: cardShadow 적용

--- src/styles/darkColors.ts
+++ src/styles/darkColors.ts
@@ -67,7 +67,7 @@ export const googleLogoColor = {
 };
 export const themeColor = {
   background: '#22272E',
-  CardShadow:
+  cardShadow:
     '0px 0.8px 2px rgba(0,0,0,.032),0px 2.7px 6.7px rgba(0,0,0,.048),0px 12px 30px rgba(0,0,0,.08)',
   backgroundOpacity900: 'rgba(34,39,46, 1)',
   backgroundOpacity: 'rgba(0 ,0 ,0, 0)',
